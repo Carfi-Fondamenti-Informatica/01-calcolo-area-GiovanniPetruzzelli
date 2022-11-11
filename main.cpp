46cddfbe264999afa6dc3d4cb1f30c91f646fbe7
GiovanniPetruzzelli <117984097+GiovanniPetruzzelli@users.noreply.github.com>
2022-11-11 17:36:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,31 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
+ //definisco le variabili
+    double a=0;
+    double b=0;
+    double c=0;
+ //utente impone variabili
 
 cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
-
+ //area triangolo
+ double trg = (a*b)/2;
+ //area quadrato
+ double qdr = a*a;
+ //area rettangolo
+ double rtg = a*b;
+ //area trapezio
+ double trp = ((a+b)*c)/2;
+ // risultati
+ 
+ //cout << trp << "\ " << endl;
+ cout << trg << endl;
+ cout << qdr << endl;
+ cout << rtg << endl;
+ cout << trp << endl;
+ 
+ return 0;
 
 }
